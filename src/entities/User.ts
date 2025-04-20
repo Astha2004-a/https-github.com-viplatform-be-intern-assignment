6f4e749ef28d142ecbfe1fd26208cb6ff1bbbb1c
Astha2004-a <asthadahiya2004@gmail.com>
2025-04-20 18:50:00 +0530

    Update User.ts

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,28 +1,99 @@
 import {
-  Entity,
-  PrimaryGeneratedColumn,
-  Column,
-  CreateDateColumn,
-  UpdateDateColumn,
+  Entity, PrimaryGeneratedColumn, Column, CreateDateColumn,
+  UpdateDateColumn, ManyToOne, OneToMany
 } from 'typeorm';
+import { User } from './User';
+import { Like } from './Like';
+import { PostHashtag } from './PostHashtag';
 
-@Entity('users')
-export class User {
+@Entity('posts')
+export class Post {
   @PrimaryGeneratedColumn('increment')
   id: number;
 
-  @Column({ type: 'varchar', length: 255 })
-  firstName: string;
+  @Column('text')
+  content: string;
 
-  @Column({ type: 'varchar', length: 255 })
-  lastName: string;
+  @ManyToOne(() => User, user => user.posts)
+  author: User;
 
-  @Column({ type: 'varchar', length: 255, unique: true })
-  email: string;
+  @OneToMany(() => Like, like => like.post)
+  likes: Like[];
+
+  @OneToMany(() => PostHashtag, postHashtag => postHashtag.post)
+  hashtags: PostHashtag[];
 
   @CreateDateColumn()
   createdAt: Date;
 
   @UpdateDateColumn()
   updatedAt: Date;
+}
+@Entity('likes')
+export class Like {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @ManyToOne(() => User, user => user.likes)
+  user: User;
+
+  @ManyToOne(() => Post, post => post.likes)
+  post: Post;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
+@Entity('follows')
+export class Follow {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @ManyToOne(() => User, user => user.following)
+  follower: User;
+
+  @ManyToOne(() => User, user => user.followers)
+  following: User;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
+@Entity('hashtags')
+export class Hashtag {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column({ type: 'varchar', length: 100, unique: true })
+  tag: string;
+
+  @OneToMany(() => PostHashtag, postHashtag => postHashtag.hashtag)
+  posts: PostHashtag[];
+}
+@Entity('post_hashtags')
+export class PostHashtag {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @ManyToOne(() => Post, post => post.hashtags)
+  post: Post;
+
+  @ManyToOne(() => Hashtag, hashtag => hashtag.posts)
+  hashtag: Hashtag;
+}
+
+@Entity('activity_logs')
+export class ActivityLog {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @ManyToOne(() => User, user => user.activityLogs)
+  user: User;
+
+  @Column({ type: 'varchar', length: 100 })
+  type: string; // e.g., 'post', 'like', 'follow', 'unfollow'
+
+  @Column('text', { nullable: true })
+  details: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
 }
